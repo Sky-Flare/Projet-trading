97620ed8411a924256560b8b44286c2b1ab3a2cc
JocelynLebaad <lebaad.jocelyn@gmail.com>
2021-04-08 15:31:35 +0200

    install bdd'

--- frontend/src/middlewares/url.js
+++ frontend/src/middlewares/url.js
@@ -1,4 +1,3 @@
-const url = 'http://ec2-34-204-76-231.compute-1.amazonaws.com/';
-//const url = 'http://localhost:3001/';
+//const url = 'http://ec2-34-204-76-231.compute-1.amazonaws.com/';
+const url = 'http://localhost:3001/';
 export default url;
- 
